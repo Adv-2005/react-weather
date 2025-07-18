d3c47eac2dac2516d4afb9e5a9fd01563e87f868
Aditya <advarshney2005@gmail.com>
2025-07-18 23:26:03 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'your-repo-name' with your actual repo name
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Adv-2005/react-weather',
+  base: '/react-weather/',
 })
